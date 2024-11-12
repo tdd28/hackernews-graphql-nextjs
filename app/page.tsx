aab5b9c9289c2d1cef68d77ce70cc278ee6a90ec
tdd28 <tdd2810@gmail.com>
2024-11-13 02:01:00 +0700

    chore: dynamic top page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import List from "@/components/List";
 import { ListType } from "@/gql/graphql";
 
+export const dynamic = 'force-dynamic'
+
 export default function Home() {
   return <List type={ListType.Topstories} />
 }
